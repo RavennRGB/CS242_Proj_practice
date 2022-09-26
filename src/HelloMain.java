92cb02dfef628b9af855f63491b4ef67f2c63fa3
larks <lark.stevens99@gmail.com>
2022-09-26 19:45:25 -0400

    #practice go brrr

---
+++ src/HelloMain.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+/** Hello Main class for demo*/
+
+public class HelloMain {
+    /**
+     * This is the maximum number*/
+
+  // public static final int MAX_NUM = 10;
+    /***
+     * Main method
+     * @param args command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+      //  int firstNum = 3;
+        System.out.println();
+    }
+}
